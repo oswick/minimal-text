55b8c2556f814413ac301ced1a48ca95922945cd
oswaldo.reggae <oswaldo.reggae@gmail.com>
2024-07-19 16:47:40 +0000

    some fixes

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/icons/logo2.svg',
+                // Agrega aquí otros recursos que deseas que estén disponibles sin conexión
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
